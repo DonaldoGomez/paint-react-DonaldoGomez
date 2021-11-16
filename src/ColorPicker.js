bd0cb36440ebe4b4b7fea825d0288c6355f35e16
DonaldoGomez <A01272435@itesm.mx>
2021-11-15 23:54:47 -0600

    Se añadió API a la lista de colores

--- src/ColorPicker.js
+++ src/ColorPicker.js
@@ -1,23 +1,47 @@
-import React from 'react';
-
-const colors = ['black', 'white', 'gray', 'red', 'orange', ' yellow', 'green', 'lime', 'navy', 'blue', 'aqua', 'purple', 'pink']
+import { useEffect, useState } from 'react';
+import './ColorPicker.css';
+import axios from 'axios';
+  
 
 function ColorPicker(props) {
+
+  const [colores, setColores] = useState([]);
+  const [estado, setEstado] = useState("IDLE");
+  const coloresAPI = [];
+
+  useEffect(()=>{
+    setEstado("LOADING");
+    axios.get("https://www.colr.org/json/colors/random/13").then( response => {
+      response.data.colors.forEach(element => {
+        coloresAPI.push(`#${element.hex}`);
+      })
+      setColores(coloresAPI);
+      setEstado("COMPLETE")
+    }).catch(e => {
+      alert("Error: "+e);
+    }) 
+  }, []) 
 
     const handleClick = (event) => {
         props.setSelectedColor(event.target.name);
     }
-
-    return (
+    if(estado === "LOADING"){
+      return(
+        <div className={"lds-ripple"}>
+          <div></div>
+          <div></div>
+        </div>
+      )
+    }else if(estado === "COMPLETE"){
+      return (
         <ul style={{ display: 'flex', width:'100%', justifyContent: 'space-evenly', listStyle: 'none' }}>
-            {/*Seleccionar un color*/}
-            {colors.map((color) => {
+            {colores.map((color, i) => {
                 const isSelected = color === props.selectedColor;
                 const borderStyle = isSelected ? '5px solid black' : '1px solid black';
 
                 return (
                     <li
-                        key={color}
+                        key={color + i}
                         onClick={handleClick}
                         name={color}
                         style={{
@@ -43,6 +67,12 @@ function ColorPicker(props) {
                 )
             })}
         </ul>
-    );
+      )
+    }else{
+      return(
+        <div>Error :(</div>
+      )
+    }
+    
 }
 export default ColorPicker;
